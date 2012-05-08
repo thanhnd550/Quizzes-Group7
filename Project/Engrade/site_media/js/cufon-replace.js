8a1b2fcd4c3d92d6a9d2f5ddbd54f3a6c2f391e2
unknown <Thanh@Thanh-Honey.(none)>
2012-05-08 15:57:34 +0700

    Signed-off-by: unknown <Thanh@Thanh-Honey.(none)>

--- Project/Engrade/site_media/js/cufon-replace.js
+++
@@ -1,4 +0,0 @@
-Cufon.replace('h2, .font2, h3, .call1', { fontFamily: 'Molengo', hover:true });
-Cufon.replace('#menu a, #slogan, .font1, .call2', { fontFamily: 'Expletus Sans', hover:true });
-Cufon.replace('.date', { fontFamily: 'Expletus Sans', hover:true, color: '-linear-gradient(#747474, #595959, #383838)' });
-
